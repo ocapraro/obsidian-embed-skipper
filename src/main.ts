7aa0e441b87bc8bb5f341a5d844e0ba59d6b7e2a
ocapraro <oscar@mcapraro.com>
2026-02-18 15:27:43 -0500

    remove templates

--- src/main.ts
+++ src/main.ts
@@ -1,99 +1,49 @@
-import {App, Editor, MarkdownView, Modal, Notice, Plugin} from 'obsidian';
-import {DEFAULT_SETTINGS, MyPluginSettings, SampleSettingTab} from "./settings";
+import {MarkdownView, Plugin, View} from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+const MAX_LOAD_CHECKS = 100;
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+/**
+ * The main plugin
+ */
+export default class EmbedSkipper extends Plugin {
 
-	async onload() {
-		await this.loadSettings();
+	async onload(): Promise<void> {
+    // When a file is opened
+    this.registerEvent(this.app.workspace.on("file-open",async (file)=>{
+      
+      // Exit out if not opening a note
+      if((file?.extension != "md"))
+        return;
+      let view = null;
+      const rawFile = await this.app.vault.cachedRead(file)
+      const splitFile = rawFile.split("\n");
 
-		// This creates an icon in the left ribbon.
-		this.addRibbonIcon('dice', 'Sample', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
+      // Repeat editor instantiation until editorFile and file are the same
+      for(let _ = 0; _ < MAX_LOAD_CHECKS; _++) {
+        await this.delay(20);
+        view = this.app.workspace.getActiveViewOfType(MarkdownView)
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status bar text');
+        // Check to see if editor has loaded
+        if(view?.editor.lineCount() !== splitFile.length)
+          continue;
+        const editorFile = view.editor.getRange(
+          {ch:0,line:0},
+          {ch:view.editor.getLine(splitFile.length-1).length-1, line:splitFile.length}
+        );
+        if(editorFile !== rawFile)
+          continue
+        splitFile.forEach(line => {
+          
+        });
+      }
+    }))
+  }
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-modal-simple',
-			name: 'Open modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				editor.replaceSelection('Sample editor command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-modal-complex',
-			name: 'Open modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-				return false;
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			new Notice("Click");
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-
+  async delay(ms: number) {
+		return new Promise(resolve => setTimeout(resolve, ms));
 	}
 
-	onunload() {
-	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData() as Partial<MyPluginSettings>);
-	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+  
 }
