3a764454aae0ea12359b24fb7d06673da4d8552c
ocapraro <oscar@mcapraro.com>
2026-02-18 19:30:46 -0500

    add CodeBlockParser

---
+++ src/CodeBlockParser.ts
@@ -0,0 +1,22 @@
+import { ContentType } from "ContentType";
+import { Parser } from "Parser";
+
+export class CodeBlockParser extends Parser{
+  parseNote(rawNote: string): number | null {
+    const splitNote = rawNote.split("\n");
+    // Exit if note doesn't start with a codeblock
+    if(splitNote.length<2 || !splitNote[0]?.includes("```"))
+      return null;
+
+    // find the end of the codeblock and return the next line
+    // or the last line in the file
+    for (let i=1; i < splitNote.length; i++)
+      if(splitNote[i]?.includes("```"))
+        return Math.min(i+1,splitNote.length-1);
+    return null;
+  }
+  getContentType(): ContentType {
+    return ContentType.CodeBlock;
+  }
+
+}
